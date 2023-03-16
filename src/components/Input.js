1982d9c820f5146bfb4e52956d813c14083499eb
Raymond <monsherr@hotmail.com>
2023-03-17 04:08:17 +0700

    test

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import {StyleSheet, TextInput, View,Text} from 'react-native';
+import {StyleSheet, TextInput, View, Text} from 'react-native';
 
-function Input({placeholder, onChangeText = text => {} , icon}) {
+function Input({placeholder, onChangeText = text => {}, icon}) {
   return (
     <View style={styles.wrapperInput}>
       <TextInput placeholder={placeholder} onChangeText={onChangeText} />
-      <View>
-       {icon}
-      </View>
+      <View>{icon}</View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   wrapperInput: {
+    height: 56,
     backgroundColor: 'white', //    <==== HERE
     borderRadius: 24,
     paddingHorizontal: 20,
@@ -26,10 +25,9 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 24,
     elevation: 10,
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   input: {},
 });
